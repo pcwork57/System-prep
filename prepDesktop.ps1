0d93da8f79a73bfcd67462339694e6b5f2f7875c
pcwork57 <39496676+pcwork57@users.noreply.github.com>
2018-05-21 11:21:03 -0700

    Create prepDesktop.ps1

---
+++ prepDesktop.ps1
@@ -0,0 +1,50 @@
+#######download lantrx desktop scrips########################
+iex ((New-Object System.Net.WebClient).DownloadString('http://help.lantrxinc.com/powershell/lantrxdesktop.ps1'))
+install-lantrxonlinescripts -desktop
+#######install package managers########################
+$scriptpolicy = get-ExecutionPolicy
+Set-ExecutionPolicy remotesigned -Force
+if(!(Test-Path "C:\ProgramData\Boxstarter\BoxstarterShell.ps1")){iex ((New-Object System.Net.WebClient).DownloadString('http://help.lantrxinc.com/powershell/bootstrapper.ps1')); get-boxstarter -Force}
+#if(!(Test-Path "C:\ProgramData\chocolatey\choco.exe")){iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))}
+#Set-ExecutionPolicy $scriptpolicy  #if need to put the execution level back to orginal
+remove-item "C:\Users\Public\desktop\Boxstarter Shell.lnk" -force
+
+#setup windows explorer to show file extenstions
+Set-WindowsExplorerOptions -EnableShowFileExtension
+
+#setup lantrx package repository
+choco source remove -n=lantrx-depo
+choco source add -n=lantrx-depo -s "'http://nupkg.lantrxinc.com/repository/App_Depo/'"
+
+
+
+#install function for removal of HP software
+
+if(!(test-path -path "c:\temp\hp-remove.csv")){
+new-item -path c:\ -name temp -itemtype directory
+New-Item -Path c:\temp\hp-remove.csv -Type file -force -Value (New-Object System.Net.WebClient).DownloadString('http://help.lantrxinc.com/powershell/hp-remove.csv')
+New-Item -Path c:\temp\hp-install.csv -Type file -force -Value (New-Object System.Net.WebClient).DownloadString('http://help.lantrxinc.com/powershell/hp-install.csv')
+New-Item -path c:\temp\packages.config -type file -force -value (New-Object System.Net.WebClient).DownloadString('http://help.lantrxinc.com/powershell/packages.xml')
+
+install-desktop -remove -csvfile c:\temp\hp-remove.csv
+install-desktop -install -csvfile c:\temp\hp-install.csv
+}
+
+
+#install preset packages
+#choco install -y c:\temp\packages.config
+choco install -y googlechrome
+choco install -y 7zip.install
+
+#clean up download files
+remove-item c:\temp\packages.config
+remove-item c:\temp\hp-remove.csv
+remove-item c:\temp\hp-install.csv
+
+#clean up desktop
+Remove-Item C:\Users\Public\Desktop\Skype*.lnk -Force
+Remove-Item 'C:\Users\Public\Desktop\HP Touchpoint*.lnk' -force
+
+
+
+
