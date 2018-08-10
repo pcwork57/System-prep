d0deedda86247836723b7d89cd13ac79382dff45
pcwork57 <39496676+pcwork57@users.noreply.github.com>
2018-08-10 15:43:53 -0700

    add notes

--- prepserver.ps1
+++ prepserver.ps1
@@ -1,3 +1,11 @@
+#package needs to be .ps1
+#choco install boxstarter
+#powershell -NoProfile -ExecutionPolicy bypass -command "Import-Module '%~dp0Boxstarter.Chocolatey\Boxstarter.Chocolatey.psd1';Invoke-ChocolateyBoxstarter %*"
+#to run from boxstarter command line with reboot
+#$cred=Get-Credential domain\username
+#Install-BoxstarterPackage -PackageName "MyPackage1","MyPackage2" -Credential $cred
+
+
 Install-WindowsUpdate -AcceptEula -full
 if (Test-PendingReboot) { Invoke-Reboot }
 
